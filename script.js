4b6a8b8841499661c6cdc3f263beeb0fb2f7b469
MeetParmar777 <manpreetparmarmp@gmail.com>
2024-12-19 23:34:52 +0530

    -

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-const cursor = document.querySelector("#cursor");
-const cursorPos = { x: 0, y: 0 };
+    const cursor = document.querySelector("#cursor");
+    const cursorPos = { x: 0, y: 0 };
 
-document.addEventListener("mousemove", (e) => {
-  cursorPos.x = e.clientX;
-  cursorPos.y = e.clientY;
+    document.addEventListener("mousemove", (e) => {
+    cursorPos.x = e.clientX;
+    cursorPos.y = e.clientY;
 
-  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-});
-
-
-
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('.navbar');
+    cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    });
 
 
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active'); // Animate icon
-    navMenu.classList.toggle('active');   // Show/hide menu
-});
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.navbar');
+
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active'); // Animate icon
+        navMenu.classList.toggle('active');   // Show/hide menu
+    });
+
+
+    function killCopy(e){
+        return false
+        }
+        function reEnable(){
+        return true
+        }
+        document.onselectstart=new Function ("return false")
+        if (window.sidebar){
+        document.onmousedown=killCopy
+        document.onclick=reEnable
+    }
